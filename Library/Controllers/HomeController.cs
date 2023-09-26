6ad585955e8e56dc4551c39aa766dd1f5c5f946f
OsiyoComputers <OsiyoComputers@WIN-1027O2O6S7J>
2023-09-26 19:32:04 +0300

    Add project files.

---
+++ Library/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Library.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static List<Book> books = new List<Book>
+        {
+            new Book
+            {
+                Id = 1,
+                Name = "Sariq devni minib",
+                Author = "X.To'xtaboyev",
+                Price = 25000
+            },
+            new Book
+            {
+                Id = 2,
+                Name = "Sariq devni minib2",
+                Author = "X.To'xtaboyev",
+                Price = 30000
+            },
+            new Book
+            {
+                Id = 3,
+                Name = "Sariq devni minib3",
+                Author = "X.To'xtaboyev",
+                Price = 35000
+            }
+        };
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(books);
+        }
+        public IActionResult Book()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Book book)
+        {
+            books.Add(book);
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ Library/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public double Price { get; set; }
+    }
+}
